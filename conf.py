e8b608e094297df637b68d58a17ad0396fb166ee
Nikolas Martens <Nikolas.M@rtens.org>
2013-04-09 12:20:06 +0200

    A start

---
+++ conf.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+AUTHOR = 'Nikolas Martens'
+SITENAME = "Nikolas.M@rtens"
+SITEURL = 'http://blog.rtens.org'
+TIMEZONE = "Europe/Berlin"
+
+GITHUB_URL = 'http://github.com/rtens/'
+REVERSE_CATEGORY_ORDER = True
+LOCALE = "C"
+DEFAULT_PAGINATION = 5
+
+SOCIAL = (('twitter', 'http://twitter.com/rtens_'),
+          ('github', 'http://github.com/rtens'),)
+
+# static paths will be copied under the same name
+STATIC_PATHS = ["img"]
+
+FILENAME_METADATA  = '(?P<date>\d{4}-\d{2}-\d{2}) (?P<title>.*)'
+ARTICLE_DIR = 'articles'
